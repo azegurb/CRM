6de8992c9b5907a367edd665a71bbb18d24a7a5f
azer.qurbanov <azer.qurbanov@cybernet.az>
2020-11-14 20:21:40 +0400

    first changes

---
+++ dist/js/app.js
@@ -0,0 +1,121 @@
+(function ($) {
+    "use strict";
+    var primarycolor = getComputedStyle(document.body).getPropertyValue('--primarycolor');
+
+//////////////////////// Window On Load //////////////////
+    $(window).on("load", function () {
+        // Animate loader off screen
+        $(".se-pre-con").fadeOut("slow");
+        ;
+    });
+
+
+/////////////////////// Loader /////////////////////
+    var angle = 0;
+    setInterval(function () {
+
+        $(".se-pre-con img")
+                .css('-webkit-transform', 'rotate(' + angle + 'deg)')
+                .css('-moz-transform', 'rotate(' + angle + 'deg)')
+                .css('-ms-transform', 'rotate(' + angle + 'deg)');
+        angle++;
+        angle++;
+        angle++;
+    }, 10);
+
+
+
+    $('.popupchat').slimScroll({
+        height: '220px',
+        color: '#fff'
+    });
+
+
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+    $('.checkall').on('click', function () {
+        $('.mail-app input:checkbox').not(this).prop('checked', this.checked);
+    });
+    
+    /**************** Chat Pop Up **********************/
+    $('.chatbutton').on('click', function () {
+        $('.chatwindow').toggle();
+        return false;
+
+    });
+    /*==============================================================
+     Sidebar 
+     ============================================================= */
+
+    $('.sidebarCollapse').on('click', function () {
+        $('body').toggleClass('compact-menu');
+        $('.sidebar').toggleClass('active');
+    });
+
+    $('.mobilesearch').on('click', function () {
+        $('.search-form').toggleClass('d-none');
+
+    });
+
+  
+    /****************************** Window Scroll ****************************/
+    $(window).on("scroll", function () {
+        /*==============================================================
+         Back To Top
+         =============================================================*/
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+
+
+/////////////////////////// Remove query string ///////////////////
+
+
+    if (uri.indexOf("?") > 0) {
+        var clean_uri = uri.substring(0, uri.indexOf("?"));
+        window.history.replaceState({}, document.title, clean_uri);
+    }
+
+})(jQuery);
+function createCookie(name, value) {
+    var now = new Date();
+    now.setTime(now.getTime() + 1 * 3600 * 1000);
+    document.cookie = name + "=" + value + ";expires=" + now.toUTCString() + "; path=/pick";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function delete_cookie(name) {
+    document.cookie = name + '=; Path=/pick; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+}
